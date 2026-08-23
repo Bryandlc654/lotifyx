5a3e67cb91d6b98c8caa55ee585a8b4bb363818b
Bryandlc654 <bdelacruz654@gmail.com>
2026-08-23 17:08:54 -0500

    fix: importar CollusionModule en LotsModule para resolver la inyeccion de CollusionService (error de arranque en Render)

--- backend/src/lots/lots.module.ts
+++ backend/src/lots/lots.module.ts
@@ -5,11 +5,12 @@ import { LotParticipant } from "./lot-participant.entity";
 import { LotRcgTier } from "./lot-rcg-tier.entity";
 import { LotBenefitApplication } from "./lot-benefit-application.entity";
 import { ConfigModule } from "../config/config.module";
+import { CollusionModule } from "../collusion/collusion.module";
 import { LotsService } from "./lots.service";
 import { LotsController } from "./lots.controller";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LotSale, LotParticipant, LotRcgTier, LotBenefitApplication]), ConfigModule],
+  imports: [TypeOrmModule.forFeature([LotSale, LotParticipant, LotRcgTier, LotBenefitApplication]), ConfigModule, CollusionModule],
   controllers: [LotsController],
   providers: [LotsService],
   exports: [LotsService],
