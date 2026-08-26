c9e2a6541cfd93eeab52c4be6b55bd9abf8d0c72
Bryandlc654 <bdelacruz654@gmail.com>
2026-08-26 00:53:21 -0500

    fix: agregar ConfigModule a ProductsModule para resolver dependencia de ConfigService

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -4,10 +4,11 @@ import { ProductsController } from "./products.controller";
 import { ProductsService } from "./products.service";
 import { Product } from "./product.entity";
 import { AuditModule } from "../audit/audit.module";
+import { ConfigModule } from "../config/config.module";
 import { AuctionMatrixModule } from "../auction-matrix/auction-matrix.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product]), AuditModule, AuctionMatrixModule],
+  imports: [TypeOrmModule.forFeature([Product]), AuditModule, ConfigModule, AuctionMatrixModule],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService],
